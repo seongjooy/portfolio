65aca4f72ed83276c7922dfef2ee324d6ddc23bf
seongjooy <seongjooyoon@gmail.com>
2022-02-22 16:53:23 +0900

    made basic website layout

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <h1>
+            About Page
+        </h1>
+        <p>
+            Lorem ipsum
+        </p>
+    </div>
+  )
+}
+
+export default about
